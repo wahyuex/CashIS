96a8944a0912b949d37f8dac599eab8ed0025ac9
wahyuex <88044555+wahyuex@users.noreply.github.com>
2023-07-31 15:25:03 +0700

    done sweetalert

--- app/Http/Controllers/ReportinController.php
+++ app/Http/Controllers/ReportinController.php
@@ -72,10 +72,12 @@ class ReportinController extends Controller
             'harga_satuan' => $obat->harga, // Ganti dengan harga satuan sesuai aplikasi Anda
             'total_harga' => $obat->harga * $jumlah_masuk, // Ganti dengan total harga sesuai aplikasi Anda
         ]);
+        $totalHargaSum = inreports::sum('total_harga');
         $reportins = inreports::all();
         return view('admin.laporanmasuk.index', [
             'pageTitle' => $pageTitle,
-            'reportins' => $reportins
+            'reportins' => $reportins,
+            'totalHargaSum' => $totalHargaSum
         ]);
         // return redirect()->route('laporanmasuk.index',['reportins' => $reportins])
         //     ->with('success', 'Stok berhasil ditambahkan.');
